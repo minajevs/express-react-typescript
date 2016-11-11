5293e157771b8b5149e65b7d10c4a70acc061e52
dexiesharp <d3x133@gmail.com>
2016-11-11 09:45:26 +0200

    added initial setup

---
+++ index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class Index extends React.Component<any,any>{
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+    render(){
+        return <div><h1>Hello World!</h1></div>;       
+    }
+}
+
+export default Index;
